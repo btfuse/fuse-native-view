e01a929aebce5ead73864fc67db39112230e3dc3
Norman Breau <norman@breautek.com>
2024-05-15 23:21:37 -0300

    ios build and release scripts

---
+++ buildIOS.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Copyright 2023 Breautek 
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source build-tools/assertions.sh
+source build-tools/DirectoryTools.sh
+source build-tools/Checksum.sh
+
+assertMac "Mac is required to build Fuse Native View iOS"
+
+if [ -z "$BTFUSE_CODESIGN_IDENTITY" ]; then
+    echo "BTFUSE_CODESIGN_IDENTITY environment variable is required."
+    exit 2
+fi
+
+echo "Building Fuse Native View iOS Framework $(cat ./ios/VERSION)..."
+
+rm -rf ./dist/ios
+mkdir -p ./dist/ios
+
+echo "Cleaning the workspace..."
+spushd ios
+    xcodebuild -quiet -workspace BTFuseNativeView.xcworkspace -scheme BTFuseNativeView -configuration Release -destination "generic/platform=iOS" clean
+    assertLastCall
+    xcodebuild -quiet -workspace BTFuseNativeView.xcworkspace -scheme BTFuseNativeView -configuration Debug -destination "generic/platform=iOS Simulator" clean
+    assertLastCall
+
+    echo "Building iOS framework..."
+    xcodebuild -quiet -workspace BTFuseNativeView.xcworkspace -scheme BTFuseNativeView -configuration Release -destination "generic/platform=iOS" build
+    assertLastCall
+    echo "Building iOS Simulator framework..."
+    xcodebuild -quiet -workspace BTFuseNativeView.xcworkspace -scheme BTFuseNativeView -configuration Debug -destination "generic/platform=iOS Simulator" build
+    assertLastCall
+
+    iosBuild=$(echo "$(xcodebuild -workspace BTFuseNativeView.xcworkspace -scheme BTFuseNativeView -configuration Release -sdk iphoneos -showBuildSettings | grep "CONFIGURATION_BUILD_DIR")" | cut -d'=' -f2 | xargs)
+    simBuild=$(echo "$(xcodebuild -workspace BTFuseNativeView.xcworkspace -scheme BTFuseNativeView -configuration Debug -sdk iphonesimulator -showBuildSettings | grep "CONFIGURATION_BUILD_DIR")" | cut -d'=' -f2 | xargs)
+
+    echo "Signing iOS build..."
+    codesign -s $BTFUSE_CODESIGN_IDENTITY --deep $iosBuild/BTFuseNativeView.framework
+
+    echo "Verifying iOS Build"
+    codesign -dvvvv $iosBuild/BTFuseNativeView.framework
+    assertLastCall
+
+    cp -r $iosBuild/BTFuseNativeView.framework.dSYM ../dist/ios/
+
+    xcodebuild -create-xcframework \
+        -framework $iosBuild/BTFuseNativeView.framework \
+        -debug-symbols $iosBuild/BTFuseNativeView.framework.dSYM \
+        -framework $simBuild/BTFuseNativeView.framework \
+        -output ../dist/ios/BTFuseNativeView.xcframework
+    assertLastCall
+spopd
+
+spushd dist/ios
+    zip -r BTFuseNativeView.xcframework.zip BTFuseNativeView.xcframework > /dev/null
+    zip -r BTFuseNativeView.framework.dSYM.zip BTFuseNativeView.framework.dSYM > /dev/null
+    sha1_compute BTFuseNativeView.xcframework.zip
+    sha1_compute BTFuseNativeView.framework.dSYM.zip
+spopd
+
+CHECKSUM=$(cat ./dist/ios/BTFuseNativeView.xcframework.zip.sha1.txt)
+
+podspec=$(<BTFuse.podspec.template)
+podspec=${podspec//\$VERSION\$/$VERSION}
+podspec=${podspec//\$CHECKSUM\$/$CHECKSUM}
+
+echo "$podspec" > BTFuseNativeView.podspec
---
+++ makeIOSRelease.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Copyright 2023 Breautek 
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source build-tools/assertions.sh
+source build-tools/DirectoryTools.sh
+
+assertMac "Mac is required for publishing"
+assertGitRepo
+assertCleanRepo
+
+if [ -z "$BTFUSE_CODESIGN_IDENTITY" ]; then
+    echo "BTFUSE_CODESIGN_IDENTITY environment variable is required."
+    exit 2
+fi
+
+VERSION="$1"
+
+assertVersion $VERSION
+assetGitTagAvailable "ios/$VERSION"
+
+echo $VERSION > ios/VERSION
+BUILD_NO=$(< "./ios/BUILD")
+BUILD_NO=$((BUILD_NO + 1))
+echo $BUILD_NO > ./ios/BUILD
+
+spushd ios/BTFuseNativeView
+    echo "// This is an auto-generated file, do not edit!" > VERSION.xcconfig
+    echo "CURRENT_PROJECT_VERSION = $BUILD_NO" >> VERSION.xcconfig
+    echo "MARKETING_VERSION = $VERSION" >> VERSION.xcconfig
+spopd
+
+./buildIOS.sh
+./test.sh
+
+git add ios/VERSION ios/BUILD ios/BTFuseNativeView/VERSION.xcconfig
+git commit -m "iOS Release: $VERSION"
+git push
+git tag -a $VERSION -m "iOS Release: $VERSION"
+git push --tags
+
+gh release create $VERSION \
+    ./dist/BTFuseNativeView.xcframework.zip \
+    ./dist/BTFuseNativeView.xcframework.zip.sha1.txt \
+    ./dist/BTFuseNativeView.framework.dSYM.zip \
+    ./dist/BTFuseNativeView.framework.dSYM.zip.sha1.txt \
+    --verify-tag --generate-notes
+
+pod spec lint BTFuseNativeView.podspec
+assertLastCall
+
+pod repo push breautek BTFuse.podspec
